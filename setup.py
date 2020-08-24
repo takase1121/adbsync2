68f743be40e43aa6920bb620704b9c3dad67b200
takase1121 <20792268+takase1121@users.noreply.github.com>
2020-08-24 09:21:57 +0800

    change entry point name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='adbsync2',
     version='0.1.0',
     entry_points={
-        'console_scripts': ['adbsync2=cli:realmain']
+        'console_scripts': ['adbsync2=cli:main']
     },
     install_requires=['iso8601~=0.1.12', 'tqdm~=4.48.2'],
     packages=find_packages()
